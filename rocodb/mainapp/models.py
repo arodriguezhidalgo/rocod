842f5781ac9f83aa638b081ddbf08e20ebb68a30
arodh <arodh91@gmail.com>
2024-01-14 16:10:24 +0000

    Stopped tracking some filews on git. Migrated db

--- rocodb/mainapp/models.py
+++ rocodb/mainapp/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 class Gym(models.Model):
     name = models.CharField(max_length=200);
-    address = models.CharField(max_length=1000);
-    country = models.CharField(max_length=200);
-    post_code=models.CharField(max_length=20);
+    address = models.CharField(max_length=1000, default="<empty>");
+    country = models.CharField(max_length=200, default="<empty>");
+    post_code = models.CharField(max_length=20, default="<empty>");
     pub_date = models.DateTimeField("date published");
+    description = models.CharField(max_length=1000, default="<empty>");
 
     def __str__(self):
         # This will return the following text to represent the object.
--- rocodb/mainapp/tests.py
+++ rocodb/mainapp/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from mainapp.models import 
 # Create your tests here.

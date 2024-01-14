ea9eae9ba442d785e71397268aef12ebc212b846
arodh <arodh91@gmail.com>
2024-01-14 16:49:22 +0000

    Implemented some basic tests

--- rocodb/mainapp/models.py
+++ rocodb/mainapp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
+from django.core.validators import MinLengthValidator
 # Create your models here.
 class Gym(models.Model):
-    name = models.CharField(max_length=200);
+    name = models.CharField(max_length=200, validators=[MinLengthValidator(1)]);
     address = models.CharField(max_length=1000, default="<empty>");
     country = models.CharField(max_length=200, default="<empty>");
     post_code = models.CharField(max_length=20, default="<empty>");
-    pub_date = models.DateTimeField("date published");
+    # pub_date = models.DateTimeField("date published", default=timezone.now());
     description = models.CharField(max_length=1000, default="<empty>");
 
     def __str__(self):
--- rocodb/mainapp/tests.py
+++ rocodb/mainapp/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
-from mainapp.models import 
+from mainapp.models import Gym
 # Create your tests here.
+
+
+class GymTestCase(TestCase):
+    def test_creates_entry_in_db(self):
+        """ Test that we can create an entry in the database."""
+        specimenName = "rocket";
+        # Create an entry.
+        Gym.objects.create(name=specimenName);
+    
+        # Retrieve the entry.
+        entry = Gym.objects.get(name=specimenName);
+        self.assertEqual(entry.name, specimenName);
+
+    def test_creates_empty_entries(self):
+        """ Test that we cannot create an entry if we don't provide the name. This
+        is the minimum required input argument."""
+        
+        Gym.objects.create()
+
+        self.assertEqual(Gym.objects.get_queryset().__len__(), 1)
+
+    def test_populates_empty_fields_with_default_values(self):
+        specimenName = "rocket";
+        defaultValue = "<empty>";
+         # Create an entry.
+        Gym.objects.create(name=specimenName);
+
+        # Retrieve the entry we just created, then assert the default values.
+        entry = Gym.objects.get(name=specimenName);
+
+        self.assertEqual(entry.address, defaultValue);
+        self.assertEqual(entry.country, defaultValue);
+        self.assertEqual(entry.post_code, defaultValue);
+        self.assertEqual(entry.description, defaultValue);
+     
